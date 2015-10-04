fc35824ac79d7e8d1d3cd6c7ef83c7ab8349aee0
George <aleksandrovgn@gmail.com>
2015-10-04 21:52:54 +0300

    in proccess adding add_category

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -49,6 +49,20 @@ def add_page(request, category_name_slug):
     except Category.DoesNotExist:
         cat = None
     if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug)
+            else:
+                print(form.errors)
+        else:
+            form = PageForm()
+        context_dict = {'form': form, 'category': cat}
+        return render(request, 'rango/add_category.html', context_dict)
 
 
 
